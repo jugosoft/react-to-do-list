47e240176a6cac1d370b4b8bc27f7c45451adac9
jugosoft <sardimka@gmail.com>
2021-09-21 21:08:53 +0400

    Redux + facts

--- react-to-do-list/redux.js
+++ react-to-do-list/redux.js
@@ -12,7 +12,8 @@ const stateInitial = {
     }
 };
 
-// Reducer - это
+// Reducer определяет, как состояние приложения изменяется в ответ на actions, отправленные в store
+// Action только описывает, что произошло, но не описывают, как изменяется store
 const reducer = (state = stateInitial, action) => {
 
     switch (action.type) {
@@ -35,17 +36,18 @@ const reducer = (state = stateInitial, action) => {
     return state;
 }
 
-// Store - 
+// Store - хранилище данных
 const store = redux.createStore(reducer);
 store.subscribe(() => {
     console.log(store.getState());
 })
 
-// Action - 
+// Action - определяют то, как мы изменяем данные
 const actionAdd = {
     type: 'ADD'
 };
 
+// Послать действие в store
 store.dispatch(actionAdd);
 
 store.dispatch({type: 'ADD', value: 100});
