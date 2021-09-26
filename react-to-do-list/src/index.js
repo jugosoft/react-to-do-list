6b58be58bef00e250cdc8cf49d7eacb844c86f1d
jugosoft <sardimka@gmail.com>
2021-09-26 21:18:04 +0400

    Redux. Included react-redux

--- react-to-do-list/src/index.js
+++ react-to-do-list/src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './redux/rootReducer';
+
+// Глобальный стор для приложения
+const store = createStore(rootReducer);
 
 // Наше приложение
 const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 ReactDOM.render(app, document.getElementById('root')
