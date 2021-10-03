d9c356498c8df11409bedd5e6dfd6952e42eb88f
jugosoft <sardimka@gmail.com>
2021-10-03 11:52:03 +0400

    Added Redux Dev Tools

--- react-to-do-list/src/index.js
+++ react-to-do-list/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './redux/rootReducer';
 import reduxThunk from 'redux-thunk';
@@ -17,8 +17,15 @@ function loggerMiddleware(store) {
   }
 }
 
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
 // Глобальный стор для приложения
-const store = createStore(rootReducer, applyMiddleware(loggerMiddleware, reduxThunk));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(loggerMiddleware, reduxThunk)));
 
 // Наше приложение
 const app = (
