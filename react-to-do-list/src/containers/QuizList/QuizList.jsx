a09c2791c7b0cb0c65b6b2f72ae0e89355f60f35
jugosoft <sardimka@gmail.com>
2021-10-12 22:53:04 +0400

    Playing With Axios

--- react-to-do-list/src/containers/QuizList/QuizList.jsx
+++ react-to-do-list/src/containers/QuizList/QuizList.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './QuizList.css';
+import axios from 'axios';
 
 export default class CreateQuiz extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            data: [1]
+        };
     }
 
     listQuizes = () => {
@@ -17,15 +21,31 @@ export default class CreateQuiz extends React.Component {
         });
     }
 
+    componentDidMount = () => {
+        axios.get('https://react-demo-5baa0-default-rtdb.europe-west1.firebasedatabase.app/data.json').then((response) => {
+            this.setState({ response });
+        });
+
+        axios.get('https://react-demo-5baa0-default-rtdb.europe-west1.firebasedatabase.app/array.json').then((response) => {
+            this.setState({ data: response.data });
+            console.log(this.state);
+        });
+
+        axios.post('https://react-demo-5baa0-default-rtdb.europe-west1.firebasedatabase.app/array.json', this.state).then((response) => {
+            this.setState({ response: response.data });
+            console.log(this.state);
+        });
+    }
+
     render() {
         return (
-            <div className="QuizList">
+            <div className="QuizList" >
                 <h1>Quiz  Listing. </h1>
-                <small>Here goes a list of quizes</small>
+                < small > Here goes a list of quizes</small >
                 <ul>
                     {this.listQuizes()}
                 </ul>
-            </div>
+            </div >
         );
     }
 }
